136e47fc741f91263782fe160f49136fd82b6517
Mansur <mans.4e4en@mail.ru>
2022-08-05 14:29:30 +0300

    Домашка

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+class Program
+{
+static void Main(string[] args)
+{
+
+ /* 
+ //Задача 2   
+int a = 5;
+int b = 7;
+if (a > b)
+{
+Console.WriteLine ("max: " + a );
+Console.WriteLine ("min: " + b );
+}
+else
+{
+   Console.WriteLine ("max: " + b );
+Console.WriteLine ("min: " + a );
+}
+*/
+
+/* 
+//Задача 4
+
+int a = 99;
+int b = 5;
+int c = 78;
+int max = a;
+
+if (max < b)
+{
+    max = b;
+}
+if (max < c)
+{
+    max = c;
+}
+Console.WriteLine("Максимальное значение: " + max);
+*/
+
+/*
+// Задача 6
+int a = 7;
+
+if (a % 2 == 0 )
+{
+    Console.WriteLine("да");
+}
+else 
+{
+Console.WriteLine ("нет");
+
+}
+*/
+
+//Задача 8
+
+int N = 15;
+int b = 2;
+
+while (b < N)
+{
+    Console.WriteLine(b);
+    b += 2;
+}
+}
+}
+}
